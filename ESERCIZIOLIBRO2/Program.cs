5c2e8a11c902b5adbd0499fe0262752b1c31e1e8
filippo giustiniani <filippo.giustiniani2004@gmail.com>
2020-02-10 19:51:47 +0100

    es

--- ESERCIZIOLIBRO2/Program.cs
+++ ESERCIZIOLIBRO2/Program.cs
@@ -7,17 +7,21 @@ namespace ESERCIZIOLIBRO2
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Inserire numeri del vettore");
+            int a = int.Parse(Console.ReadLine());
+            List<int> list = new List<int>(a);
             Console.WriteLine("Inserisci numeri");
             int n = int.Parse(Console.ReadLine());
+            Random r = new Random();
             int mass = int.MaxValue;
             int min = int.MinValue;
-            Random r = new Random();
-            List<int> list = new List<int>();
+            List<int> crescente = new List<int>();
             for (int i = 0; i < n; i++)
             {
                 list.Add(r.Next(min, mass));
-                min = list[i];
+                min = crescente[i];
             }
+            Console.ReadLine();
         }
     }
 }
